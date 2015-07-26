2176a62a524d3d155aee237df84cbbe388830c73
Sofia Nguy <sofianguy@gmail.com>
2015-07-25 22:48:13 -0700

    almost finished part 1. Commit before sleep.

--- skills.sql
+++ skills.sql
@@ -1,39 +1,48 @@
 -- 1. Select all columns for all brands in the Brands table.
 SELECT * FROM Brands;
 
+
 -- 2. Select all columns for all car models made by Pontiac in the Models table.
 SELECT * FROM Models WHERE brand_name = "Pontiac";
+
 
 -- 3. Select the brand name and model 
 --    name for all models made in 1964 from the Models table.
 SELECT brand_name, name FROM Models WHERE year = 1964;
 
+
 -- 4. Select the model name, brand name, and headquarters for the Ford Mustang 
 --    from the Models and Brands tables.
+SELECT Models.name, Models.brand_name, Brands.headquarters 
+FROM Models JOIN Brands 
+ON Models.brand_name=Brands.name
+WHERE Models.brand_name='Ford' AND Models.name='Mustang';
 
--- SELECT name, brand_name, headquarters FROM Models, Brands 
---     WHERE brand_name = "Ford" AND name = "Mustang";
 
 -- 5. Select all rows for the three oldest brands 
 --    from the Brands table (Hint: you can use LIMIT and ORDER BY).
+SELECT * FROM Brands ORDER BY Brands.founded LIMIT 3;
 
 
 -- 6. Count the Ford models in the database (output should be a **number**).
+SELECT count(*) FROM Models WHERE brand_name='Ford';
 
 
 -- 7. Select the **name** of any and all car brands that are not discontinued.
+SELECT name FROM Brands WHERE discontinued IS NULL;
 
 
 -- 8. Select rows 15-25 of the DB in alphabetical order by model name.
-
+SELECT * FROM Models WHERE 
 
 -- 9. Select the **brand, name, and year the model's brand was 
 --    founded** for all of the models from 1960. Include row(s)
 --    for model(s) even if its brand is not in the Brands table.
 --    (The year the brand was founded should be ``null`` if 
 --    the brand is not in the Brands table.)
-
-
+SELECT Models.brand_name, Brands.name, Brands.founded FROM 
+Brands.name == Ford
+Models.brand_name == Ford
 
 -- Part 2: Change the following queries according to the specifications. 
 -- Include the answers to the follow up questions in a comment below your
